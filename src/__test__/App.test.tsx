6879d8641329948a791fc5762769c6834cd05b1a
김강현 <bebekh1216@gmail.com>
2023-08-31 00:31:05 +0900

    test: App test코드 추가

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -2,8 +2,28 @@ import { render, screen } from "@testing-library/react";
 
 import App from "../App";
 
-test("renders learn react link2", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Rendering", () => {
+  test("화면에 타이틀이 정상적으로 떠야한다", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/TODO List/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("화면에 입력을 위한 input이 정상적으로 떠야한다", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(/할 일을 입력해주세요/i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test("화면에 추가 버튼이 정상적으로 떠야한다", () => {
+    render(<App />);
+    const buttonElement = screen.getByText(/추가/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  test("화면에 리스트가 정상적으로 떠야한다", () => {
+    render(<App />);
+    const listElement = screen.getByTestId("todo-list");
+    expect(listElement).toBeInTheDocument();
+  });
 });
